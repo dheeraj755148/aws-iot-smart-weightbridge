edbd5c0beb573c2539191dd30537ab39b2470cdc
Dheeraj Dayakaran <61353080+dheeraj755148@users.noreply.github.com>
2022-07-23 16:32:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as AWS from "aws-sdk";
 
-AWS.config.update({
-  region: "ap-south-1",
-  endpoint: "dynamodb.ap-south-1.amazonaws.com",
-  accessKeyId: "AKIAQCQCGYXPAO2MD5XS",
-  secretAccessKey: "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",
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,8 +13,6 @@ root.render(
   </React.StrictMode>
 );
 
-/* Access: AKIAQCQCGYXPAO2MD5XS */
-/* Secret : POIdX+ml1Vq+nQVc+zT1+5+tp+I2gkrfQ33XVvnq */
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
